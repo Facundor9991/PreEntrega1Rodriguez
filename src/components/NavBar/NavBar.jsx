651a36557017bdd0c10d653f7f8203f98df7b5e3
Facundor9991 <facundor99@outlook.com>
2023-05-13 17:26:25 -0300

    Actualizacion 3/3

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,11 @@ import { Categorias } from "./Categorias/Categorias";
 import { CartWidget } from "../CartWidget/CartWitdget.jsx";
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
-        <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon" />
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark"  >
+        <div className="container-fluid"  >
+            <button className="navbar-toggler" type="button"  data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"  >
+               
+                <Categorias />
             </button>
             <div className="collapse navbar-collapse">
                 <Categorias />
